04e2449c5473cada2c36ad4e6feb94ef30c24383
luciobian <lbiancardi91@gmail.com>
2020-01-31 19:11:22 -0300

    fix: Corrección link de archivo css.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/styles.css') }}" />
 </head>
 <body>
     <div id="app">

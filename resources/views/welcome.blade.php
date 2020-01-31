288731b8361d7ef8e0a0273b949cfa6152d03609
luciobian <lbiancardi91@gmail.com>
2020-01-30 22:52:09 -0300

    feat: Archivo de estilos linkeado.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        {{ URL::asset('css/css.css'); }}    
         <title>Laravel</title>
 
         <!-- Fonts -->

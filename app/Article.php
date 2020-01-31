6b0b0b87690d1b27578a7053ddab231f41995548
luciobian <lbiancardi91@gmail.com>
2020-01-30 22:49:45 -0300

    feat: Clave primaria asignada a modelo Article.

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $guarded = [];
+
+    
+    public function getKeyName()
+    {
+        return 'id';    
+    }
 }

d00caae063a01fbff599a80a5842ea6e5c930449
luciobian <lbiancardi91@gmail.com>
2020-02-02 17:37:02 -0300

    feat: Policy para Article creado.

---
+++ app/Policies/ArticlePolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\Article;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ArticlePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the given article can be deleted by the user.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return bool
+     */
+    public function delete(User $user, Article $article)
+    {
+        return $user->id === $article->user_id || $user->role_id === 1;
+    }
+}

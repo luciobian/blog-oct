27cd40420f9e8e20486f88045ce82685427f0417
luciobian <lbiancardi91@gmail.com>
2020-02-02 15:14:43 -0300

    feat: Relación entre Image y Article agregada.

--- app/Article.php
+++ app/Article.php
@@ -8,7 +8,23 @@ class Article extends Model
 {
     protected $guarded = [];
 
-    
+
+    protected $with = ['images'];
+
+    /**
+     * Relación con modelo Image
+     *
+     * @return object
+     */    
+    public function images(){
+        return $this->hasOne("App\Image", "id");
+    }
+
+    /**
+     * Clave para las rutas
+     *
+     * @return string
+     */
     public function getRouteKey()
     {
         return 'id';    
--- app/Image.php
+++ app/Image.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Relación con el modelo Article
+     *
+     * @return object
+     */
+    public function articles(){
+        return $this->belongsTo("App\Article", "image_id");
+    }
 }

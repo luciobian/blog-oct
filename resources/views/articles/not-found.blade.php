1128972cc1aa78d0a94ddeeb7b86d4cfc44cb167
luciobian <lbiancardi91@gmail.com>
2020-02-03 20:14:17 -0300

    feat: Vista para artículos no encontrados creada.

---
+++ resources/views/articles/not-found.blade.php
@@ -0,0 +1,8 @@
+<div class="not-found">
+    <span class="text-center">
+        <h3>No se encontraron artículos</h3>
+    </span>
+    <object type="image/svg+xml" data="{{asset("/img/not-found.svg")}}" width="100%" height="375vh">
+        Your browser does not support SVG.
+    </object>
+</div>

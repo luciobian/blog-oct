7af8169c7307bd2d6b03c2f562e4e39f6c52d12b
luciobian <lbiancardi91@gmail.com>
2020-02-03 19:06:36 -0300

    feat: Input para búsqueda de artículos incluido.

--- resources/views/articles/order.blade.php
+++ resources/views/articles/order.blade.php
@@ -1,23 +1,21 @@
-<span class="text-right">
-    <div class="dropdown show">
-        <a class="btn btn-link dropdown-toggle" 
-            href="#" 
-            role="button" 
-            id="dropdownOrder" 
-            data-toggle="dropdown"
-            aria-haspopup="true" 
-            aria-expanded="false">
-            Ordenar
-        </a>
+<div class="d-flex justify-content-between align-center">
+    @include('articles.filter')
+    <span>
+        <div class="dropdown show">
+            <a class="btn btn-link dropdown-toggle" href="#" role="button" id="dropdownOrder" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Ordenar
+            </a>
 
-        <div class="dropdown-menu" aria-labelledby="dropdownOrder">
-            <a class="dropdown-item" href="#">Últimos</a>
-            <a class="dropdown-item" href="#">Populares</a>
-            @auth
-            <a class="dropdown-item" href="#">Mis artículos</a>
-            @endauth
+            <div class="dropdown-menu" aria-labelledby="dropdownOrder">
+                <a class="dropdown-item" href="#" selected>Últimos</a>
+                <a class="dropdown-item" href="#">Populares</a>
+                @auth
+                <a class="dropdown-item" href="#">Mis artículos</a>
+                @endauth
 
+            </div>
         </div>
-    </div>
-</span>
+    </span>
+</div>
 <hr>

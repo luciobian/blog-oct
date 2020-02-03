216fffc79ec5dd3d3a1f325e2b6a75d8f0a07fa3
luciobian <lbiancardi91@gmail.com>
2020-02-02 22:51:45 -0300

    feat: Operaciones para editar imagen agregadas.

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use App\Image;
 use Illuminate\Http\Request;
 
@@ -76,9 +77,29 @@ class ImageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Article $article)
     {
-        //
+        $image = Image::whereId($article->images->id)->first();
+
+        if($image){
+            // $image->delete();
+            $imageName = time().'.'.request()->image->getClientOriginalExtension();    
+            $image = $image->update([
+                "path"=>time().'.'.request()->image->getClientOriginalExtension(),
+            ]);    
+            request()->image->move(public_path('images'), $imageName);
+    
+        }
+
+        $article->title = $request->get('title');
+
+        if($article->isDirty()){
+            $image->update([
+                "alternative"=>$request->get('title'),
+            ]);
+        }
+
+        return $image;
     }
 
     /**

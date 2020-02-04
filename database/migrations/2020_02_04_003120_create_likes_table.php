fa8fc20f76132e9dabb1c2a3c9474c917be5d280
luciobian <lbiancardi91@gmail.com>
2020-02-03 22:20:31 -0300

    feat: Migración para la tabla like agregada.

---
+++ database/migrations/2020_02_04_003120_create_likes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('articles_id');
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references("id")->on("users")->onDelete('cascade');
+            $table->foreign('articles_id')->references("id")->on('articles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}

91431944ab0a58d9e96c609b3d2c4f71444c2b0f
luciobian <lbiancardi91@gmail.com>
2020-02-18 19:06:41 -0300

    feat: Relaciones agregadas.

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * Relacion con modelo de usuarios.
+     *
+     * @return void
+     */
+    public function users()
+    {
+        return $this->belongsTo("App\User", 'id');
+    }
+
+    /**
+     * Relacion con modelo de articulos.
+     *
+     * @return void
+     */
+    public function articles()
+    {
+        return $this->belongsTo("App\Articles", 'id');
+    }
 }

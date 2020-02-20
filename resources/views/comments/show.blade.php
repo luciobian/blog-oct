649ed73aa19017f9076781e2df1becdde29d2429
luciobian <lbiancardi91@gmail.com>
2020-02-20 19:56:57 -0300

    feat(refactor): Visualizaciones de comentarios refactorizado.

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -2,9 +2,9 @@
     <div class="card">
         <div class="card-header" id="comments">
             <h5 class="mb-0">
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false"
                     aria-controls="collapseOne">
-                    <h5 class="mb-0">Comentarios ({{$comments->count()}})</h5>
+                    <h5 class="mb-0">Comentarios ({{ $comments->count()}})</h5>
                 </button>
             </h5>
         </div>
@@ -12,10 +12,13 @@
         <div id="collapseOne" class="collapse show" aria-labelledby="comments" data-parent="#accordion">
             <div class="card-body">
 
-                
+                @if ( $comments->count()==0)
+                 <h5>Se el primero en <a href="">comentar</a> este artículo.</h5>
+                @else
                 @foreach ($comments as $comment)
                 <li class="list-group-item">
                     <div class="d-flex justify-content-between">
+                       {{ $comments->count()}}
                         <h5>{{$comment->users->name}}</h5>
                         <span class="text-muted">{{$comment->created_at->diffForHumans()}}</span>
                     </div>
@@ -25,7 +28,8 @@
                 </li>
                 <hr>
                 @endforeach
-                
+                @endif
+
             </div>
         </div>
     </div>
